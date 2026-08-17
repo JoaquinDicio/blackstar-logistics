e538f6c9a6bb1552bf909826d6c6160cd628f3b1
joaquindicio <joacodicio@gmail.com>
2026-08-17 20:14:31 -0300

    robots.txt and sitemap plugin added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blackstar-logistics.com",
+
+  integrations: [sitemap()],
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
